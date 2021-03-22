7af8879e4b69d9e50084ff1c81e9561edbc27bcd
NickHuangbeauty <a92224733@gmail.com>
2021-03-22 17:14:48 +0800

    added my first golang file

---
+++ code_samples/012-updated-version/002-Practice_myself/002-01_GolangStructure.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Control Flow:
+	// 1. Sequence
+	// 2. for loop; iterative
+	// 3. conditions
+	fmt.Println("This is my first time to learn GOlang")
+
+	foo()
+
+	bar()
+}
+
+func foo() {
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func bar() {
+	fmt.Println("This is the end of for loop")
+}
